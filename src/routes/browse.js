5c6ff8c771ff321d7f6b57d71c88186f59734543
neslihankara <neslihankara40@gmail.com>
2021-03-02 16:25:41 +0300

    Display races in profile

--- src/routes/browse.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-router.post('/browse', (req, res) => {
-  res.send('browse', { title: 'run the world' })
-})
-
-module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const Race = require('../models/race')
+
 const router = express.Router()
 
 /* GET home page. */
@@ -8,9 +10,17 @@ router.get('/', (req, res) => {
 })
 
 router.get('/profile', (req, res) => {
-  const createdRaces = ['heyo', 'this', 'is', 'a', 'mock', 'array', '<3']
-  const admittedRaces = ['also', 'this', 'one']
-  const attemptedRaces = ['this', 'one', 'as well']
+  const createdRaces = [new Race('marathon A', 42, 'road', 25, 'f', '13:30', '6 hours')] // the races that have the user whose isCreator property is true for that race in the races array
+  const admittedRaces = [
+    new Race('race B', 25, 'trail', 22, 'm', '15:30', '5 hours'),
+    new Race('race C', 5, 'road', 30, 'f', '16:30', '3 hours'),
+    new Race('half-marathon D', 26, 'road', 23, 'f-m', '13:30', '6 hours'),
+  ]
+  const attemptedRaces = [
+    new Race('marathon A', 42, 'road', 25, 'f', '13:30', '6 hours'),
+    new Race('race C', 5, 'road', 30, 'f', '16:30', '3 hours'),
+    new Race('half-marathon D', 26, 'road', 23, 'f-m', '13:30', '6 hours'),
+  ]
 
   res.render('profile', { createdRaces, admittedRaces, attemptedRaces })
 })
