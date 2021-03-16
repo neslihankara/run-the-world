df1176c56b466e83a756ef97864d10caf5fd32d0
neslihankara <neslihankara40@gmail.com>
2021-03-16 13:18:54 +0300

    Fetch users

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -1,14 +1,27 @@
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'Profile',
   components: {},
   data() {
-    return {}
+    return {
+      users: []
+    }
+  },
+  // this part is broken
+  async created() {
+    this.users = await this.fetchUsers()
+  },
+  methods: {
+    ...mapActions(['fetchUsers'])
   }
 }
 </script>
 
 <template lang="pug">
-.profile
-  p here comes a profile
+  .profile
+    p hi
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{ user.name }}
 </template>
