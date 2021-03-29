eb5c7e40a625e669ee9e3b5ef32e8bef1d497547
neslihankara <neslihankara40@gmail.com>
2021-03-29 21:44:28 +0300

    Fix gender property for race in RaceCard

--- frontend/src/components/race-card.vue
+++ frontend/src/components/race-card.vue
@@ -17,7 +17,7 @@ export default {
         h5.card-title {{ race.name }}
         p.card-text {{ race.kilometers }} km
         p.card-text {{ race.requiredRunnerAge }} years of age
-        p.card-text For {{ race.requiredGender }} runners
+        p.card-text For {{ race.requiredRunnerGender }} runners
         p.card-text On the {{  race.terrain }}
         p.card-text Starts at {{ race.startTime }}
         a.btn.btn-primary(href='/') Find some friends at race.runners
