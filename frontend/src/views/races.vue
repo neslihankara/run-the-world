0aa8795e9f0e05aacff17c1fb3f02d2435fbaa5e
neslihankara <neslihankara40@gmail.com>
2021-03-29 20:01:15 +0300

    Add race view

---
+++ frontend/src/views/races.vue
@@ -0,0 +1,26 @@
+<script>
+import { mapActions } from 'vuex'
+import RaceCard from '@/components/race-card.vue'
+
+export default {
+  name: 'Races',
+  components: { RaceCard },
+  data() {
+    return {
+      race: null
+    }
+  },
+  async created() {
+    this.race = await this.fetchRaces()
+    console.log('here?')
+  },
+  methods: {
+    ...mapActions(['fetchRaces'])
+  }
+}
+</script>
+
+<template lang="pug">
+    .about
+        RaceCard(:race="race" v-if="race")
+</template>
