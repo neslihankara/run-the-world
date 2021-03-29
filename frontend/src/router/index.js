cf4aee2f4cd00b93951261b21cbf3bd7733a8508
neslihankara <neslihankara40@gmail.com>
2021-03-29 20:03:07 +0300

    Define the path for race view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/home.vue'
 import Profile from '../views/profile.vue'
 import Login from '../views/login.vue'
+import Races from '../views/races.vue'
 Vue.use(VueRouter)
 
 export default function init(store) {
@@ -24,6 +25,11 @@ export default function init(store) {
         path: '/login',
         name: 'Login',
         component: Login
+      },
+      {
+        path: '/races',
+        name: 'Races',
+        component: Races
       }
     ]
   })
