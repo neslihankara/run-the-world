7222e9433d276b0bf6bbb3443c0110779638168f
neslihankara <neslihankara40@gmail.com>
2021-04-08 16:15:25 +0300

    Fix the form

--- frontend/src/views/create-race.vue
+++ frontend/src/views/create-race.vue
@@ -45,31 +45,37 @@ export default {
 </script>
 
 <template lang="pug">
-.createRace
-    form( @submit="createRace")
-      h1 Create a new race
-      label(for="name") Name:&nbsp;
-          input(v-model="name" id="name" type="text" placeholder="Race's name" required)
-      label(for="kilometers") Kilometers:&nbsp;
-          input(v-model="kilometers" id="kilometers" type="number" placeholder="Race's length" required)
-      label(for="terrain") Terrain:&nbsp;
-          input(v-model="terrain" id="terrain" type="text" placeholder="Race's terrain type" required)
-      label(for="requiredRunnerGender") Gender:&nbsp;
-          input(v-model="requiredRunnerGender" id="requiredRunnerGender" type="text" placeholder="Race's specificity" required)
-      label(for="requiredRunnerAge") Age:&nbsp;
-          input(v-model="requiredRunnerAge" id="requiredRunnerAge" type='number' placeholder="Race's age span" required)
-      label(for="startTime") Starting time:&nbsp;
-          input(v-model="startTime" id="startTime" type="text" placeholder="Race's start time" required)
-      br
-      input(type="submit" value="Create")
-    div(v-if="backendError") {{ backendError }}
-    br
-    div Looking for a race? <router-link to="/races">Browse races</router-link>
+div
+  form(@submit.prevent="submitRace")
+    h1 Create a new race
+    label(for="name") Name:&nbsp;
+      input(v-model="race.name" id="name" type="text" placeholder="Race's name" required)
+    label(for="kilometers") Kilometers:&nbsp;
+      input(v-model="race.kilometers" id="kilometers" type="number" placeholder="Race's length" required)
+    label(for="terrain") Terrain:&nbsp;
+      input(v-model="race.terrain" id="terrain" type="text" placeholder="Race's terrain type" required)
+    label(for="requiredRunnerGender") Gender:&nbsp;
+      input(v-model="race.requiredRunnerGender" id="requiredRunnerGender" type="text" placeholder="Race's specificity" required)
+    label(for="requiredRunnerAge") Age:&nbsp;
+      input(v-model="race.requiredRunnerAge" id="requiredRunnerAge" type='number' placeholder="Race's age span" required)
+    label(for="startTime") Starting time:&nbsp;
+      input(v-model="race.startTime" id="startTime" type="text" placeholder="Race's start time" required)
+    button(type="submit") Create
+  .text-error(v-if="backendError") {{ backendError }}&nbsp;
+    p Make sure you are logged in.
+  div
+    | Looking for a race?&nbsp;
+    router-link(to="/races") Browse races
 </template>
 
 <style lang="scss" scoped>
 label {
   display: block;
   margin: 1rem 0;
+}
+
+.text-error {
+  color: red;
+  font-weight: bold;
 }
 </style>
