c3b474474ff5a649aeeaf52d417e455ea35736fa
neslihankara <neslihankara40@gmail.com>
2021-04-30 08:20:31 +0300

    Remove live chat from profile page

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -7,25 +7,8 @@ export default {
   components: {
     RaceCard
   },
-  data() {
-    return {
-      users: [],
-      message: ''
-    }
-  },
-  async created() {
-    this.users = await this.fetchSession()
-  },
-  methods: {
-    ...mapActions(['fetchSession', 'goLive', 'sendMessageToLiveStream', 'joinStream']),
-    sendMessage(e) {
-      e.preventDefault()
-      this.sendMessageToLiveStream(this.message)
-      this.message = ''
-    }
-  },
   computed: {
-    ...mapState(['currentLiveStream', 'liveStreams', 'user', 'liveStreamMessages'])
+    ...mapState(['user'])
   }
 }
 </script>
@@ -36,29 +19,6 @@ export default {
       .row
         .col-12.col-md-6.col-lg-4(v-for="attendance in user.attendances")
           RaceCard(:race="attendance.race" v-if="attendance.race")
-    .container.socket-container
-      p hi
-      div(v-for="user in users")
-        router-link(:to="`/users/${user._id}`") {{ user.name }}
-      div(v-if="liveStreams.length")
-        h2 Live streams
-        div(v-for="stream in liveStreams")
-          p {{ stream }}
-          button(@click="joinStream(stream)") Join stream
-      button(@click="goLive") Go live
-      div(v-if="currentLiveStream")
-        h3 live stream
-        .messages
-          .message(v-for="message in liveStreamMessages")
-            p
-              span {{ message.author }}:&nbsp;
-              span {{ message.body }}
-        form(@submit="sendMessage")
-          input(type="text" v-model="message")
-          input(type="submit" value="Send message")
 </template>
 
-<style lang="scss" scoped>
-.container.socket-container {
-}
-</style>
+<style lang="scss" scoped></style>
