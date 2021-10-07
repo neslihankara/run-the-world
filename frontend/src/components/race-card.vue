3f6963a37c88dfebf96911b17edec14f4801e869
neslihankara <neslihankara40@gmail.com>
2021-10-08 02:09:13 +0300

    Improve race card

--- frontend/src/components/race-card.vue
+++ frontend/src/components/race-card.vue
@@ -27,17 +27,24 @@ export default {
 </script>
 
 <template lang="pug">
-.container
-  .card
-    .card-body
-      h5.card-title {{ race.name }}
-      p.card-text {{ race.kilometers }} km
-      p.card-text {{ race.requiredRunnerAge }} years of age
-      p.card-text For {{ race.requiredRunnerGender }} runners
-      p.card-text On the {{  race.terrain }}
-      p.card-text Starts at {{ race.startTime }}
-      button.btn.btn-primary(@click='apply' :disabled="race.attendances.some(attendance => attendance.user._id == user._id)") Apply for this race!
-      a.btn.btn-primary(href='/') See the results
+.races
+  .container
+    .card
+      .card-body
+        .banner
+          img(src="img/banners/race-card-banner.jpg")
+        .race-info
+          .race-name
+            h1.card-title {{ race.name }}
+          .info
+            p.card-text Dest {{ race.kilometers }} km
+            p.card-text Age {{ race.requiredRunnerAge }}
+            p.card-text Sex {{ race.requiredRunnerGender }}
+            p.card-text Type {{  race.terrain }}
+            p.card-text Time {{ race.startTime }}
+        .buttons
+          button.btn.btn-primary(@click='apply' :disabled="race.attendances.some(attendance => attendance.user._id == user._id)") Apply
+          a.btn.btn-primary(href='/') Results
 </template>
 
 <style lang="scss" scoped>
@@ -47,11 +54,34 @@ export default {
   border-radius: 0.3rem;
   background: pink;
   margin-bottom: 5rem;
+  p {
+    font-size: 25px;
+  }
+}
+.race-info {
+  .race-name {
+    h1 {
+      font-size: 50px;
+    }
+    margin-left: -50px;
+  }
+  .info {
+    display: flex;
+    direction: column;
+    justify-content: space-around;
+  }
 }
 .btn {
   background: rgb(238, 126, 144);
   border: 0.5px solid purple;
   padding: 0.5rem;
   margin: 0.5rem;
+}
+.banner {
+  img {
+    height: 200px;
+    width: 100%;
+    object-fit: cover;
+  }
 }
 </style>
